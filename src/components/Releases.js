f54df5bc522add520db22a8fef1a233d88c729a2
Olks jobb <26792432+Olks95@users.noreply.github.com>
2020-11-29 15:14:34 +0100

    Finished Releases page with embedded youtube player

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+
+const youtubeLink = "https://www.youtube.com/embed/tgbNymZ7vqY"
+
 function Releases() {
 	return (
-		<div>
-			I am the latest and greatest release!
-		</div>
+		<>
+			<div className="Releases-container">
+				<div className="Releases-header">
+					<h1>Green Leaves - Official Music Video</h1>
+				</div>
+				<div className="Releases-body">
+					<iframe className="Releases-video" src={youtubeLink} allowFullScreen>
+					</iframe>
+				</div>
+				<div className="Releases-footer">
+					<h2 className="cta">
+						Watch more videos
+					</h2>
+					<a className="btn btn-lg btn-dark cta-youtube" href="https://www.youtube.com">
+						<FontAwesomeIcon icon={faYoutube} className="cta-youtube-icon" size="2x"/>Visit YouTube
+					</a>
+				</div>
+			</div>
+		</>
 	)
 }
 
