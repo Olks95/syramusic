5b50759ddfaf32e1852b1d89b53ffbc62c19c636
Olks jobb <26792432+Olks95@users.noreply.github.com>
2020-12-05 13:06:13 +0100

    Added Coming Soon on Release video

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -15,6 +15,10 @@ function Releases() {
 					<iframe id="Releases-video" type="text/html" src={youtubeLink} frameBorder="0" allowFullScreen allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  title="Newest Syra Video">
 						Browser Not Compatible.
 					</iframe>
+					<div className="Releases-coming-soon Shows-container">
+						<h1>COMING SOON</h1>
+						<h2>18th December</h2>
+					</div>
 				</div>
 				<div className="Releases-footer">
 					<h2 className="cta">
