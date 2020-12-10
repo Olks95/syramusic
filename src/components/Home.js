37d7ab6c7ca8e60f86330f66c2dce3b74472e67d
Olks jobb <26792432+Olks95@users.noreply.github.com>
2020-12-10 15:19:51 +0100

    Re-populated site with newest content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,47 @@
 import React from 'react'
-import slideOneImg from '../images/green-leaves-1.jpg'
-import slideTwoImg from '../images/instagram-current.jpg'
-import slideThreeImg from '../images/Teardrop10.png'
-import slideFourImg from '../images/green-leaves-2.jpg'
-import slideFiveImg from '../images/green-leaves-lyric.jpg'
+import slideGreenLeavesOne from '../images/green-leaves-1.jpg'
+import slideInstaOne from '../images/instagram-current.jpg'
+import slideYouTubeOne from '../images/Teardrop10.png'
+import slideGreenLeavesTwo from '../images/green-leaves-2.jpg'
+import slideYouTubeTwo from '../images/green-leaves-lyric.jpg'
 
 import Carousel from './Carousel.js'
 
 
 const slides = [{
-	src: slideOneImg,
-	title: 'Green Leaves',
-	subtitle: 'Debut Single, out 18th Dec 2020',
-	alt: 'Green Leaves Album Cover',
-	href: "https://www.spotify.com/",
+	src: slideInstaOne,
+	title: 'Follow SYRA',
+	subtitle: '@syra.music on Instagram',
+	alt: 'Instagram',
+	href: "https://www.instagram.com/syra.music/",
 	id: 1001
 }, {
-	src: slideTwoImg,
-	title: 'Follow Me',
-	subtitle: '@syra.music on Instagram',
-	alt: 'Latest Instagram image',
-	href: "https://www.instagram.com/syra.music/",
+	src: slideYouTubeOne,
+	title: 'Subscribe to SYRA',
+	subtitle: 'on YouTube',
+	alt: 'YouTube Profile Image',
+	href: "https://www.youtube.com/channel/UCjXACzv_0uZw8Jqtya4CKqA",
 	id: 1002
 },{
-	src: slideThreeImg,
-	title: 'Subscribe to SYRA',
-	subtitle: 'on YouTube',
-	alt: "YouTube Profile Image",
-	href: "https://www.youtube.com/channel/UCjXACzv_0uZw8Jqtya4CKqA",
+	src: slideGreenLeavesOne,
+	title: 'Green Leaves',
+	subtitle: 'Pre-save now',
+	alt: "Green Leaves Album Cover",
+	href: "https://distrokid.com/hyperfollow/syra3/green-leaves",
 	id: 1003
 },{
-	src: slideFourImg,
-	title: 'Green Leaves',
-	subtitle: 'Debut Single, out 18th Dec 2020',
-	alt: 'Green Leaves Picture',
-	href: "https://www.spotify.com/",
+	src: slideYouTubeTwo,
+	title: 'Subscribe to SYRA',
+	subtitle: 'for more videos',
+	alt: 'YouTube Link',
+	href: "https://www.youtube.com/channel/UCjXACzv_0uZw8Jqtya4CKqA",
 	id: 1004
 }, {
-	src: slideFiveImg,
-	title: 'Subscribe to SYRA',
-	subtitle: 'on YouTube',
-	alt: 'Lyric Video Cover',
-	href: "https://www.youtube.com/channel/UCjXACzv_0uZw8Jqtya4CKqA",
+	src: slideGreenLeavesTwo,
+	title: 'Green Leaves',
+	subtitle: 'Debut Single out 18th Dec 2020',
+	alt: 'Spotify Pre-save Link',
+	href: "https://distrokid.com/hyperfollow/syra3/green-leaves",
 	id: 1005
 }]
 
