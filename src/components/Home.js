34f22a3277f1b79deb1203f6e57c12e7364b00c5
olks95 <olks.sev@gmail.com>
2021-01-28 14:25:06 +0100

    New song coming soon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import slideGreenLeavesOne from '../images/green-leaves-1.jpg'
 import slideInstaOne from '../images/instagram-current.jpg'
 import slideYouTubeOne from '../images/carouselOption.png'
-import slideGreenLeavesTwo from '../images/green-leaves-2.jpg'
+import wgly from '../images/wgly.jpg'
 import slideYouTubeTwo from '../images/Teardrop10.png'
+import slideGreenLeaves from '../images/green-leaves-1.jpg'
 
 import Carousel from './Carousel.js'
 
 
 const slides = [{
-	src: slideInstaOne,
-	title: 'Follow SYRA',
-	subtitle: '@syra.music on Instagram',
-	alt: 'Instagram',
-	href: "https://www.instagram.com/syra.music/",
+	src: slideGreenLeaves,
+	title: 'Green Leaves',
+	subtitle: 'Click here to stream now',
+	alt: "Green Leaves Album Cover",
+	href: "https://distrokid.com/hyperfollow/syra3/green-leaves",
 	id: 1001
 }, {
 	src: slideYouTubeOne,
@@ -23,11 +23,11 @@ const slides = [{
 	href: "https://www.youtube.com/watch?v=XFDgPyesLqU&ab_channel=SYRA",
 	id: 1002
 },{
-	src: slideGreenLeavesOne,
-	title: 'Green Leaves',
-	subtitle: 'Click here to stream now',
-	alt: "Green Leaves Album Cover",
-	href: "https://distrokid.com/hyperfollow/syra3/green-leaves",
+	src: wgly,
+	title: "Who's Gonna Love You",
+	subtitle: 'Coming Soon',
+	alt: "Who's Gonna Love You teaser",
+	href: "https://www.instagram.com/p/CKhZ_lzjcqE/",
 	id: 1003
 },{
 	src: slideYouTubeTwo,
@@ -37,11 +37,11 @@ const slides = [{
 	href: "https://open.spotify.com/artist/0gnCReeV7AEe4i0960GB0e",
 	id: 1004
 }, {
-	src: slideGreenLeavesTwo,
-	title: 'Green Leaves',
-	subtitle: 'Debut Single, listen now',
-	alt: 'Distrokid Link',
-	href: "https://distrokid.com/hyperfollow/syra3/green-leaves",
+	src: slideInstaOne,
+	title: 'Follow SYRA',
+	subtitle: '@syra.music on Instagram',
+	alt: 'Instagram',
+	href: "https://www.instagram.com/syra.music/",
 	id: 1005
 }]
 
